62e1bfe7e2aa7936024b8b5f6e385fbf525f8d41
armandosneto <armandosneto@outlook.com>
2021-03-01 16:48:37 -0300

    All API is ready

--- src/Services/SendMailService.ts
+++ src/Services/SendMailService.ts
@@ -7,7 +7,7 @@ class SendMailService {
 
     private client: Transporter;
 
-    constructor(){
+    constructor() {
 
         nodemailer.createTestAccount().then((account) => {
             const transporter = nodemailer.createTransport({
@@ -27,27 +27,29 @@ class SendMailService {
 
     }
 
-    async execute(to: string, subject: string, variables: object,path: string) {
+    async execute(to: string, subject: string, variables: object, path: string) {
 
         const templateFileContent = fs.readFileSync(path).toString("utf8");
 
         const mailTemplateParse = handlebars.compile(templateFileContent);
-        
+
         const html = mailTemplateParse(variables);
 
-        const message = await this.client.sendMail({
-                to,
-                subject,
-                html:html,
-                from: "NPS <noreplay@nps.com.br>"
-            }, 
-        );
+        this.client.sendMail({
+            to,
+            subject,
+            html: html,
+            from: "NPS <noreplay@nps.com.br>"
+        },
+            (err, info) => {
+                console.log('Message sent: %s', info.messageId);
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-        console.log('Message sent: %s', message.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+            }
+        );
 
     }
 
 }
 
-export default new SendMailService(); 
+export default new SendMailService();
